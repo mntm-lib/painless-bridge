7762fc06efcd414ba3dca69823df6a3df9072f82
eolme <im@petrov.engineer>
2021-11-07 22:15:45 +0400

    Implement storage plugin

---
+++ src/plugins/storage.ts
@@ -0,0 +1,17 @@
+import type { VKBridgeMethodResult, VKBridgeSend } from '../types/data.js';
+
+export const pluginStorage = (send: VKBridgeSend): VKBridgeSend => {
+  return (method, params) => {
+    if (method === 'VKWebAppStorageGet') {
+      return send(method, params).then((payload) => {
+        return Promise.all((payload as VKBridgeMethodResult<'VKWebAppStorageGet'>).keys.map((pair) => {
+          return send('VKWebAppStorageSet', pair);
+        })).then(() => {
+          return payload;
+        });
+      });
+    }
+
+    return send(method, params);
+  };
+};
